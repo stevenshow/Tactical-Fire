71853de1caea098b3559577944f285617bfbd660
Steven Schoebinger <steven.schoebinger@gmail.com>
2022-08-16 14:41:30 -0600

    unused vars

--- src/App.js
+++ src/App.js
@@ -1,29 +1,28 @@
-import logo from './images/logo.svg'
-import './styles/App.css'
-import FilteredTable from './components/Table'
-import BasicCard from './components/Card'
-import SimpleAccordion from './components/Accordion'
+import "./styles/App.css";
+import FilteredTable from "./components/Table";
+import BasicCard from "./components/Card";
+import SimpleAccordion from "./components/Accordion";
 
 function App() {
   return (
-    <div className='App'>
-      <header className='App-header'>
+    <div className="App">
+      <header className="App-header">
         <h1>Tactical Fire</h1>
-        <BasicCard name='Boots'>
+        <BasicCard name="Boots">
           <SimpleAccordion>
-            <FilteredTable name='Hiking' />
-            <FilteredTable name='Work' />
+            <FilteredTable name="Hiking" />
+            <FilteredTable name="Work" />
           </SimpleAccordion>
         </BasicCard>
-        <BasicCard name='Pants'>
+        <BasicCard name="Pants">
           <SimpleAccordion>
-            <FilteredTable name='Hiking' />
-            <FilteredTable name='Work' />
+            <FilteredTable name="Hiking" />
+            <FilteredTable name="Work" />
           </SimpleAccordion>
         </BasicCard>
       </header>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Table.js
+++ src/components/Table.js
@@ -1,90 +1,90 @@
-import { useCallback, useState, useEffect } from 'react'
-import Box from '@mui/material/Box'
-import { DataGrid, GridToolbar } from '@mui/x-data-grid'
-import Snackbar from '@mui/material/Snackbar'
-import Alert from '@mui/material/Alert'
+import { useCallback, useState } from "react";
+import Box from "@mui/material/Box";
+import { DataGrid, GridToolbar } from "@mui/x-data-grid";
+import Snackbar from "@mui/material/Snackbar";
+import Alert from "@mui/material/Alert";
 
 const useFakeMutation = () => {
   return useCallback(
     (product) =>
       new Promise((resolve, reject) =>
         setTimeout(() => {
-          if (product.name?.trim() === '') {
+          if (product.name?.trim() === "") {
             reject(
               new Error("Error while saving product: quantity can't be empty.")
-            )
+            );
           } else {
-            resolve({ ...product, quantity: product?.quantity })
+            resolve({ ...product, quantity: product?.quantity });
           }
         }, 200)
       ),
     []
-  )
-}
+  );
+};
 
 const rows = [
-  { id: 1, name: 'North Face', size: 12, quantity: 2 },
-  { id: 2, name: 'Columbia', size: 11, quantity: 2 },
-  { id: 3, name: 'Trailhead', size: 10, quantity: 2 },
-  { id: 4, name: 'Brooks', size: 10, quantity: 2 },
-  { id: 5, name: 'Salomon', size: 14, quantity: 2 },
-  { id: 6, name: 'Merrell', size: 15, quantity: 2 },
-  { id: 7, name: 'Keen', size: 12, quantity: 2 },
-  { id: 8, name: 'Oboz', size: 13, quantity: 2 },
-  { id: 9, name: 'Altra', size: 11, quantity: 2 },
-]
+  { id: 1, name: "North Face", size: 12, quantity: 2 },
+  { id: 2, name: "Columbia", size: 11, quantity: 2 },
+  { id: 3, name: "Trailhead", size: 10, quantity: 2 },
+  { id: 4, name: "Brooks", size: 10, quantity: 2 },
+  { id: 5, name: "Salomon", size: 14, quantity: 2 },
+  { id: 6, name: "Merrell", size: 15, quantity: 2 },
+  { id: 7, name: "Keen", size: 12, quantity: 2 },
+  { id: 8, name: "Oboz", size: 13, quantity: 2 },
+  { id: 9, name: "Altra", size: 11, quantity: 2 },
+];
 const columns = [
   {
-    field: 'id',
-    headerName: 'ID',
+    field: "id",
+    headerName: "ID",
     flex: 1,
     hide: true,
     sortable: false,
     filterable: false,
   },
   {
-    field: 'name',
-    headerName: 'NAME',
+    field: "name",
+    headerName: "NAME",
     flex: 2,
   },
   {
-    field: 'size',
-    headerName: 'SIZE',
+    field: "size",
+    headerName: "SIZE",
     flex: 1,
   },
   {
-    field: 'quantity',
-    headerName: 'QUANTITY',
-    type: 'number',
+    field: "quantity",
+    headerName: "QUANTITY",
+    type: "number",
     editable: true,
     flex: 1,
-    align: 'center',
-    headerAlign: 'center',
+    align: "center",
+    headerAlign: "center",
   },
-]
+];
 
 const FilteredTabled = ({ data }) => {
-  const mutateRow = useFakeMutation()
-  const [snackbar, setSnackbar] = useState(null)
-  const handleCloseSnackbar = () => setSnackbar(null)
+  const mutateRow = useFakeMutation();
+  const [snackbar, setSnackbar] = useState(null);
+  const handleCloseSnackbar = () => setSnackbar(null);
 
   const processRowUpdate = useCallback(
     async (newRow) => {
       // Make the HTTP request to save in the backend
-      const response = await mutateRow(newRow)
-      console.log({ response })
+      const response = await mutateRow(newRow);
+      console.log({ response });
       setSnackbar({
         children: `Quantity successfully updated to ${response.quantity}`,
-        severity: 'success',
-      })
-      return response
+        severity: "success",
+      });
+      return response;
     },
     [mutateRow]
-  )
+  );
 
   const handleProcessRowUpdateError = useCallback((error) => {
-    setSnackbar({ children: error.message, severity: 'error' })
-  }, [])
+    setSnackbar({ children: error.message, severity: "error" });
+  }, []);
 
   return (
     <>
@@ -92,7 +92,7 @@ const FilteredTabled = ({ data }) => {
         sx={{
           height: 400,
           width: 1,
-          backgroundColor: 'white',
+          backgroundColor: "white",
         }}
       >
         <DataGrid
@@ -116,7 +116,7 @@ const FilteredTabled = ({ data }) => {
       {!!snackbar && (
         <Snackbar
           open
-          anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
+          anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
           onClose={handleCloseSnackbar}
           autoHideDuration={6000}
         >
@@ -124,7 +124,7 @@ const FilteredTabled = ({ data }) => {
         </Snackbar>
       )}
     </>
-  )
-}
+  );
+};
 
-export default FilteredTabled
+export default FilteredTabled;
